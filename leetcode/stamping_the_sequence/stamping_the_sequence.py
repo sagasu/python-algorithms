6e52c4391115ab83437e535f29f2ef2cdbd267a7
Mateusz Kopij <sagasu@gmail.com>
2021-03-31 19:13:22 +0100

    Stamping The Sequence

---
+++ leetcode/stamping_the_sequence/stamping_the_sequence.py
@@ -0,0 +1,48 @@
+class Solution:
+    def movesToStamp(self, stamp: str, target: str) -> List[int]:
+        ans = []
+        N = len(target)
+        M = len(stamp)
+
+        stamp = list(stamp)
+        old_target = target
+        target = list(target)
+
+        def match(offset):
+            count = 0
+            for i in range(M):
+                if stamp[i] != target[i + offset] and target[i + offset] != "*":
+                    return False
+                if target[i + offset] != "*":
+                    count +=1
+            return count > 0
+
+        while True:
+            found = False
+
+            for start in range(N - M + 1):
+                if match(start):
+                    found = True
+                    for i in range(start, start + M):
+                        target[i] = "*"
+                    ans.append(start)
+                    break
+
+            print(target, ans)
+            if all(x == "*" for x in target):
+                break
+
+            if not found:
+                return []
+
+        ans.reverse()
+        constructed = [""] * N
+        for index in ans:
+            for k in range(M):
+                constructed[index + k] = stamp[k]
+
+        print(ans, "".join(constructed))
+        if "".join(constructed) == old_target:
+            return ans
+        return []
+        
