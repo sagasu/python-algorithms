ad22e6996b9fee29923ef45bf72d044a3af7189f
Mateusz Kopij <sagasu@gmail.com>
2021-11-08 18:55:59 +0000

    Unique Binary Search Trees

--- leetcode/unique_binary_search_trees/unique_binary_search_trees.py
+++ leetcode/unique_binary_search_trees/unique_binary_search_trees.py
@@ -1,7 +1,7 @@
 class Solution:
     def numTrees(self, n):
-        if n == 0: return 1
-        res = 0
-        for i in range(1, n+1):
-            res += self.numTrees(i-1)*self.numTrees(n-i)
-        return res
+            c = 1
+            for i in range(1,n):
+                #Catalan number
+                c = c * 2 * (2 * i + 1) / (i + 2)
+            return int(c)
